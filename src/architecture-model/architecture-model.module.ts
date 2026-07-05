266aa2bf87c86b03053d58e0da80f84aa8d51dad
sowahm808 <sowahm808@gmail.com>
2026-07-05 18:45:50 -0500

    Build production NestJS backend foundation

---
+++ src/architecture-model/architecture-model.module.ts
@@ -0,0 +1,1 @@
+import { Module } from '@nestjs/common';import { ArchitectureModelController } from './architecture-model.controller';import { ProjectsModule } from '../projects/projects.module';@Module({imports:[ProjectsModule],controllers:[ArchitectureModelController]})export class ArchitectureModelModule{}
---
+++ src/artifacts/artifacts.module.ts
@@ -0,0 +1,1 @@
+import { Module } from '@nestjs/common';import { BullModule } from '@nestjs/bullmq';import { ArtifactsController } from './artifacts.controller';import { ProjectsModule } from '../projects/projects.module';@Module({imports:[ProjectsModule,BullModule.registerQueue({name:'artifact-generation'})],controllers:[ArtifactsController]})export class ArtifactsModule{}
